ae4fbd8ef2ac9e13723da5a02f4de0232e37ed9f
boshido <boshido@windowslive.com>
2013-07-10 23:52:42 +0700

    Status OK

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'VOftWHuQNl1tHvgjOErYkeX7zNZRLqQ0',
 
 	/*
 	|--------------------------------------------------------------------------
